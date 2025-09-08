fd50a22f5f0e07a61ec3f200f91016a9596dbcca
Barbu Revencu <barbu.revencu@gmail.com>
2025-09-08 11:23:14 +0200

    Week 1 Materials

---
+++ Week-1/Exercises/Exercise-4.py
@@ -0,0 +1,148 @@
+################################################################################
+"""
+Recommended readings: 
+  Chapter on dictionaries: https://automatetheboringstuff.com/3e/chapter7.html 
+  Iterating through dictionaries: https://realpython.com/iterate-through-dictionary-python/
+"""
+################################################################################
+
+"""
+Exercise 4.1
+
+Task:
+------
+Print the sum of the values in the dictionary.
+"""
+
+dct = {'a': 3, 'b': 7, 'c': -2, 'd': 10, 'e': 5}
+
+print("Exercise 4.1")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.2
+
+Task:
+------
+Print the key that has the largest value in dct.
+"""
+
+print("Exercise 4.2")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.3
+
+Task:
+------
+Create a new dictionary with the squares of all the values in dct.
+"""
+
+print("Exercise 4.3")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.4
+
+Task:
+------
+Print only the keys in dct whose values are even numbers.
+"""
+
+print("Exercise 4.4")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.5
+
+Task:
+------
+Create a new dictionary that swaps the keys and values in dct.
+"""
+
+print("Exercise 4.5")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.6
+
+Task:
+------
+Count the number of times each letter appears in the string 'ccctcctttttcc'
+and print the resulting dictionary.
+"""
+
+s = 'ccctcctttttcc'
+
+print("Exercise 4.6")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.7
+
+Task:
+------
+Given the dictionary of responsesMapping = {'j':'jazz', 'p':'pop'},
+and the string responses = 'jjjpjjpppppjj',
+print the list of corresponding words.
+"""
+
+responsesMapping = {'j':'jazz','p':'pop'}
+responses = 'jjjpjjpppppjj'
+
+print("Exercise 4.7")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.8
+
+Task:
+------
+Merge the following two dictionaries into one:
+{'a': 1, 'b': 2} and {'c': 3, 'd': 4}
+"""
+
+print("Exercise 4.8")
+
+pass
+
+print("---")
+
+"""
+Exercise 4.9
+
+Task:
+------
+For the dictionary {'apple': 3, 'banana': 2, 'orange': 5},
+print each key followed by its value, one per line, like:
+apple: 3
+banana: 2
+orange: 5
+"""
+
+print("Exercise 4.9")
+
+pass
+
+print("---")
