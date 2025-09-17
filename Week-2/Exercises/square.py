17681831fd8661ad3c0abd7731eb9375cd544c25
Julia Zibrev <jzibrev@gmail.com>
2025-09-17 22:13:00 +0200

    Added answers to Exercise 1 square.py

--- Week-2/Exercises/square.py
+++ Week-2/Exercises/square.py
@@ -1,23 +1,40 @@
+#create a script that displays a fixation cross inside 
+#a blue square of length 50 for half a second, 
+# then removes the fixation cross and 
+# displays only the blue square of length 50 
+# until a key is pressed
+
+# Import the main modules of expyriment
 from expyriment import design, control, stimuli
 
-control.set_develop_mode()
-
+# Create an object of class Experiment: This stores the global settings of your experiment & handles the data file, screen, and input devices
 exp = design.Experiment(name = "Square")
 
+# Initialize the experiment: Must be done before presenting any stimulus
 control.initialize(exp)
 
-fixation = stimuli.FixCross()
+# Create a fixation cross (color, size, and position will take on default values)
+fixation = stimuli.FixCross() # At this stage the fixation cross is not yet rendered
 
-square = stimuli.Rectangle((50,50),colour=blue)
+# Create a 50px-radius circle
+square = stimuli.Rectangle(size=(50,50),colour=(0,0,255))
 
+# Start running the experiment
 control.start(subject_id=1)
 
-fixation.present(clear=True, update=True)
+square.present(clear=True, update=False)
+fixation.present(clear=False, update=True)
 
+
+# Leave it on-screen for 500 ms
 exp.clock.wait(500)
 
+# Remove the cross 
 square.present(clear=True, update=True)
 
+# Leave it on-screen until a key is pressed
 exp.keyboard.wait()
 
+# End the current session and quit expyriment
 control.end()
+
