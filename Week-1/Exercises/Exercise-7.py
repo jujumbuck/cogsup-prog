5bf616e13a6b1d2fb4d3eccc828d3ea9a783d399
Julia Zibrev <jzibrev@gmail.com>
2025-09-20 17:22:43 +0200

    added comments to exercise 7

--- Week-1/Exercises/Exercise-7.py
+++ Week-1/Exercises/Exercise-7.py
@@ -16,7 +16,8 @@ print("think of a number between 1 and 100")
 
 
 def guessing():
-    lower_bound=1
+    #creating upper and lower bounds to have the range of the guess
+    lower_bound=1 
     upper_bound=100
     #random first guess
     guess = randint(lower_bound,upper_bound)
@@ -25,17 +26,17 @@ def guessing():
 
     while True:
         print('is your number lower or higher than', guess)
-        feedback = input("Enter 'higher', 'lower', or 'correct': ").lower()
+        feedback = input("Enter 'higher', 'lower', or 'correct': ").lower() #asking user to enter feedback about guess
 
         if feedback == "higher":
-            lower_bound = guess+1
-            guess = randint(lower_bound,upper_bound)
+            lower_bound = guess+1 #updating lower bound to be in the range of previous guess but higher
+            guess = randint(lower_bound,upper_bound) #guessing a random number in the bound of the last guess+1 up to the upper bound whoch hasnt changed
         elif feedback == "lower":
-            upper_bound= guess-1
-            guess = randint(lower_bound,upper_bound)
+            upper_bound= guess-1 #updating upper bound to be in the range of previous guess but lower
+            guess = randint(lower_bound,upper_bound) #guessing a random number in the range of the lower bound that hasnt changed up to the previous guess -1
         elif feedback == "correct":
-            print('i correctly guessed', guess)
-            break
+            print('i correctly guessed', guess) 
+            break #break loop so that the guessing stops because correct
         else:
             print('pls type higher,lower or correct')
 
